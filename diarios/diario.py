c025c98f4ddfcd7d49f22a9e0c6933f1c720ddcc
camilalopes <kmilalopes10@gmail.com>
2019-02-19 12:42:29 -0300

    gerando hash md5 do pdf

--- diarios/diario.py
+++ diarios/diario.py
@@ -1,18 +1,5 @@
-#from selenium import webdriver
-#import time
-
-#DOWNLOAD_PATH = "C:\\Users\\Camila\\Downloads"
-
-#options = webdriver.ChromeOptions()
-#preferences = {"download.default_directory": DOWNLOAD_PATH, "directory_upgrade": True, "safebrowsing.enabled": True }
-#options.add_experimental_option("prefs", preferences)
-#driver = webdriver.Chrome(options=options)
-
-#driver.get("http://inter03.tse.jus.br/sadJudDiarioDeJusticaConsulta/diario.do?action=buscarDiarios&page=diarioPageLastList.jsp&voDiarioSearch.tribunal=TSE&voDiarioSearch.calendario=false&voDiarioSearch.dataPubIni=01/02/2018&voDiarioSearch.dataPubFim=01/02/2018")
-
-#injected_javascript = 'document.getElementById("id").value = 86737; document.getElementById("tribunal").value = "TSE"; document.getElementById("captchaValidacao").value = "ok"; document.getElementById("formDocumentoDownload").submit();'
-
 import requests
+import hashlib
 
 URL = 'http://inter03.tse.jus.br/sadJudDiarioDeJusticaConsulta/diario.do?action=downloadDiario'
 data = {"id":"86737",
@@ -20,6 +7,8 @@ data = {"id":"86737",
         "captchaValidacao":"ok"}
 req = requests.post(URL, params = data)
 
-arq = open('C:/Users/Camila/Downloads/arquivo.pdf', 'wb')
-arq.write(req.content)
+#hash do pdf
+print (hashlib.md5(req.content).hexdigest())
+
+
 arq.close()

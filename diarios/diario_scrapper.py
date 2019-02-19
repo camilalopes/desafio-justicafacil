9f7a7b54510cfd4096d1eba93dbccf770d80e913
camilalopes <kmilalopes10@gmail.com>
2019-02-19 14:54:33 -0300

    salvando o resultado

--- diarios/diario_scrapper.py
+++ diarios/diario_scrapper.py
@@ -6,6 +6,9 @@ URL = 'http://inter03.tse.jus.br/sadJudDiarioDeJusticaConsulta/'
 URL_PDF = URL+'diario.do?action=downloadDiario'
 URL_DATA = URL+'diario.do?action=buscarDiarios&page=diarioPageLastList.jsp&voDiarioSearch.tribunal=TSE&voDiarioSearch.calendario=false'
 
+#Arquivo que salva os resultados
+RESULTADO_PATH = 'C:/Users/Camila/Desktop/resultado_hashes.csv'
+
 '''Retorna a lista com o id dos pdfs a serem baixados de acordo com a data'''
 def lista_id_pdfs(data):
     param = {"voDiarioSearch.dataPubIni": data,
@@ -44,9 +47,20 @@ def lista_hash(data):
     lista_hashes = lista_hash_pdf(pdf_ids)
     return lista_hashes
 
+'''Salva os resultados de maneira persistente em uma planilha'''
+def salva_csv(data, lista_hash):
+    arq = open(RESULTADO_PATH, 'w') # 'a' tbm pode ser usado se quiser manter o conteudo do arq anterior
+    linhas_de_texto = ['data', 'hash pdf']
+    for hash in lista_hash:
+        arq.writelines(';'.join([data, hash]))
+    arq.close()
+
 def main():
     data = '01/02/2018'
-    print(lista_hash(data))
+    #data = '14/06/2017'
+    hashes = lista_hash(data)
+    print(hashes)
+    salva_csv(data, hashes)
 
 if __name__ == "__main__":
 	main()

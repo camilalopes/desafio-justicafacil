da450f269f440f3acc31bce6e1db44392d04207a
camilalopes <kmilalopes10@gmail.com>
2019-02-19 12:12:40 -0300

    baixando pdf via post com request - melhor solucao

--- diarios/diario.py
+++ diarios/diario.py
@@ -1,13 +1,25 @@
-from selenium import webdriver
-import time
+#from selenium import webdriver
+#import time
 
-DOWNLOAD_PATH = "C:\\Users\\Camila\\Downloads"
+#DOWNLOAD_PATH = "C:\\Users\\Camila\\Downloads"
 
-options = webdriver.ChromeOptions()
-preferences = {"download.default_directory": DOWNLOAD_PATH, "directory_upgrade": True, "safebrowsing.enabled": True }
-options.add_experimental_option("prefs", preferences)
-driver = webdriver.Chrome(options=options)
+#options = webdriver.ChromeOptions()
+#preferences = {"download.default_directory": DOWNLOAD_PATH, "directory_upgrade": True, "safebrowsing.enabled": True }
+#options.add_experimental_option("prefs", preferences)
+#driver = webdriver.Chrome(options=options)
 
-driver.get("http://inter03.tse.jus.br/sadJudDiarioDeJusticaConsulta/diario.do?action=buscarDiarios&page=diarioPageLastList.jsp&voDiarioSearch.tribunal=TSE&voDiarioSearch.calendario=false&voDiarioSearch.dataPubIni=01/02/2018&voDiarioSearch.dataPubFim=01/02/2018")
+#driver.get("http://inter03.tse.jus.br/sadJudDiarioDeJusticaConsulta/diario.do?action=buscarDiarios&page=diarioPageLastList.jsp&voDiarioSearch.tribunal=TSE&voDiarioSearch.calendario=false&voDiarioSearch.dataPubIni=01/02/2018&voDiarioSearch.dataPubFim=01/02/2018")
 
-injected_javascript = 'document.getElementById("id").value = 86737; document.getElementById("tribunal").value = "TSE"; document.getElementById("captchaValidacao").value = "ok"; document.getElementById("formDocumentoDownload").submit();'
+#injected_javascript = 'document.getElementById("id").value = 86737; document.getElementById("tribunal").value = "TSE"; document.getElementById("captchaValidacao").value = "ok"; document.getElementById("formDocumentoDownload").submit();'
+
+import requests
+
+URL = 'http://inter03.tse.jus.br/sadJudDiarioDeJusticaConsulta/diario.do?action=downloadDiario'
+data = {"id":"86737",
+        "tribunal":"TSE",
+        "captchaValidacao":"ok"}
+req = requests.post(URL, params = data)
+
+arq = open('C:/Users/Camila/Downloads/arquivo.pdf', 'wb')
+arq.write(req.content)
+arq.close()

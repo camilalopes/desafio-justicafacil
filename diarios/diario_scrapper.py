900cb760bfe5f0990bace17dec734796b6a631e3
camilalopes <kmilalopes10@gmail.com>
2019-02-26 15:35:49 -0300

    corrigindo o padrao de comentarios das funcoes para one-line docstrings do python

--- diarios/diario_scrapper.py
+++ diarios/diario_scrapper.py
@@ -11,18 +11,18 @@ URL_DATA = URL+'diario.do?action=buscarDiarios&page=diarioPageLastList.jsp&voDia
 #Arquivo onde o resultado pode ser salvado
 RESULTADO_PATH = os.path.abspath(os.path.dirname(""))+'\\resultado_hashes.csv'
 
-'''Transforma a data passada em uma string no formato brasileiro de datas'''
 def formata_data(data):
+    '''Transforma a data passada em uma string no formato brasileiro de datas'''
     data_str = data.strftime('%d/%m/%Y')
     return data_str
 
-'''Verifica se a data é válida'''
 def valida_data(data):
+    '''Verifica se a data é válida'''
     if data > date.today():
         raise ValueError('Uma data inválida foi passada')
 
-'''Retorna a lista com o id dos pdfs a serem baixados de acordo com a data passada'''
 def lista_id_pdfs(data):
+    '''Retorna a lista com o id dos pdfs a serem baixados de acordo com a data passada'''
     valida_data(data)
     data_str = formata_data(data)
     param = {"voDiarioSearch.dataPubIni": data_str,
@@ -47,8 +47,8 @@ def lista_id_pdfs(data):
         pdf_ids.append(id)
     return pdf_ids
 
-'''Retorna uma lista de hashes para uma lista de pdfs passada'''
 def lista_hash_pdf(pdf_ids):
+    '''Retorna uma lista de hashes para uma lista de pdfs passada'''
     lista_hash = []
     for pdf_id in pdf_ids:
         param = {"id": pdf_id,
@@ -64,14 +64,14 @@ def lista_hash_pdf(pdf_ids):
         lista_hash.append(hash)
     return lista_hash
 
-'''Retorna uma lista de hashes para uma determinada data passada'''
 def lista_hash(data):
+    '''Retorna uma lista de hashes para uma determinada data passada'''
     pdf_ids = lista_id_pdfs(data)
     lista_hashes = lista_hash_pdf(pdf_ids)
     return lista_hashes
 
-'''Salva os resultados de maneira persistente em uma planilha'''
 def salva_csv(data, lista_hash):
+    '''Salva os resultados de maneira persistente em uma planilha'''
     data_str = formata_data(data)
     arq = open(RESULTADO_PATH, 'w') # 'a' tbm pode ser usado se quiser manter o conteudo do arq anterior
     linhas_de_texto = ['data', 'hash pdf']
@@ -82,7 +82,7 @@ def salva_csv(data, lista_hash):
 def main():
     data = date.today()
     hashes = lista_hash(data)
-    print(hashes)
+    #print(hashes)
     salva_csv(data, hashes) #salva o resultado
 
 if __name__ == "__main__":
